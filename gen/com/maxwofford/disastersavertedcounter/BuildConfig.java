ff7e01368a48436487ebd0baf78522327d7e7ff4
MaxWofford <max.speed.wofford@gmail.com>
2014-03-09 14:52:59 -0700

    Uploading initial project

---
+++ gen/com/maxwofford/disastersavertedcounter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.maxwofford.disastersavertedcounter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
